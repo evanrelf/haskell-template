734d3fea6acd4c76ddee1ee60e1c426d6ede4311
Evan Relf <erelf@panosoft.com>
2019-12-30 10:00:11 -0800

    Remove unnecessary let binding from shell.nix

--- shell.nix
+++ shell.nix
@@ -1,8 +1,5 @@
 { pkgs ? import ./nixpkgs.nix {} }:
 
-let
-  template = import ./default.nix {};
-in
-  pkgs.lib.overrideDerivation template.env (old: {
-    buildInputs = with pkgs; old.buildInputs ++ [ cabal-install ghcid hlint ];
-  })
+(import ./default.nix {}).env.overrideAttrs (old: {
+  buildInputs = with pkgs; old.buildInputs ++ [ cabal-install ghcid hlint ];
+})
