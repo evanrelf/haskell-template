e6911ce2c8341d954e8315772a35e40fddd6876d
Evan Relf <evan@evanrelf.com>
2020-03-28 17:06:50 -0700

    Automatically patch Haskell packages

--- default.nix
+++ default.nix
@@ -3,14 +3,41 @@ let
     haskellPackages = pkgsOld.haskellPackages.override (old: {
       overrides =
         let
-          extension =
-            (pkgsNew.haskell.lib.packagesFromDirectory {
-              directory = ./nix/haskell-packages;
-            });
+          directory = ./nix/haskell-packages;
+
+          overridePackages =
+            pkgsNew.haskell.lib.packagesFromDirectory { inherit directory; };
+
+          patchPackages = haskellPkgsNew: haskellPkgsOld:
+            let
+              packageNames =
+                builtins.map
+                  (pkgsNew.lib.removeSuffix ".patch")
+                  (builtins.filter
+                    (pkgsNew.lib.hasSuffix ".patch")
+                    (builtins.attrNames (builtins.readDir directory)));
+
+              patchPackage = name:
+                if haskellPkgsOld ? "${name}" then
+                  pkgsNew.haskell.lib.overrideCabal haskellPkgsOld."${name}" (oldDrv: {
+                    patches =
+                      (if oldDrv ? patches then oldDrv.patches else [])
+                      ++ [ (directory + "/${name}.patch") ];
+                  })
+                else
+                  throw "'${name}' is not in the Haskell package set";
+            in
+              builtins.listToAttrs
+                (builtins.map
+                  (name: { inherit name; value = patchPackage name; })
+                  packageNames);
         in
-          pkgsNew.lib.composeExtensions
+          pkgsNew.lib.fold
+            pkgsNew.lib.composeExtensions
             (old.overrides or (_: _: {}))
-            extension;
+            [ overridePackages
+              patchPackages
+            ];
     });
   };
 
