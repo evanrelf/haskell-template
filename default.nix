b1fe1f23518bcab42190e3f28504260e0ea43b68
Evan Relf <evan@evanrelf.com>
2020-11-30 19:54:14 -0800

    Pin newer Hackage/`all-cabal-hashes` explicitly

--- default.nix
+++ default.nix
@@ -9,6 +9,10 @@ let
           patches = (oldCabal.patches or []) ++ [ ./nix/patches/relude.patch ];
         };
       };
+      hackage = {
+        rev = "52415450270fb5d146097c36e74d1117ba0e4fe4";
+        sha256 = "0cc7ls5awhb83jfm8kcaskglgqala32q5s8j87frz0f6wx57gbl4";
+      };
     };
 
 
