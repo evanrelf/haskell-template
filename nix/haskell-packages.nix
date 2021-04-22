0c69f49b11b75967a66baf39580971c38e38ce6c
Evan Relf <evan@evanrelf.com>
2021-04-21 21:54:23 -0700

    Move import into `let` binding

--- nix/haskell-packages.nix
+++ nix/haskell-packages.nix
@@ -1,7 +1,12 @@
 pkgsFinal: pkgsPrev:
 
-import ./lib/override-haskell-packages.nix {
+let
+  overrideHaskellPackages = attrs:
+    import ./lib/override-haskell-packages.nix attrs pkgsFinal pkgsPrev;
+
+in
+overrideHaskellPackages {
   packages = {
     "template" = pkgsPrev.nix-gitignore.gitignoreSource [ ../.nixignore ] ../.;
   };
-} pkgsFinal pkgsPrev
+}
