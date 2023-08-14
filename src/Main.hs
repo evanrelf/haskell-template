b48c8aa8abb964f240508f12c1836ea608cbc78a
Evan Relf <evan@evanrelf.com>
2023-08-14 14:35:46 -0700

    Add `optparse-applicative`

--- src/Main.hs
+++ src/Main.hs
@@ -1,6 +1,18 @@
+{-# LANGUAGE ApplicativeDo #-}
+
 module Main (main) where
 
+import Options.Applicative qualified as Options
 import Template qualified
 
+getOptions :: IO Template.Options
+getOptions = do
+  let parserPrefs = Options.prefs mempty
+  let parserInfo =
+        Options.info (Options.helper <*> Template.parseOptions) mempty
+  Options.customExecParser parserPrefs parserInfo
+
 main :: IO ()
-main = Template.main
+main = do
+  options <- getOptions
+  Template.main options
--- src/Template.hs
+++ src/Template.hs
@@ -1,4 +1,17 @@
-module Template (main) where
+module Template
+  ( Options (..)
+  , parseOptions
+  , main
+  )
+where
 
-main :: IO ()
-main = putTextLn "Hello world!"
+import Options.Applicative qualified as Options
+
+data Options = Options {}
+
+parseOptions :: Options.Parser Options
+parseOptions = do
+  pure Options{}
+
+main :: Options -> IO ()
+main _options = putTextLn "Hello world!"
