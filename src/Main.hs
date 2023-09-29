1294f40732d693b1baf25c728187f0f6f6e143e9
Evan Relf <evan@evanrelf.com>
2023-09-28 20:10:14 -0700

    Move all code out of executable

--- src/Main.hs
+++ src/Main.hs
@@ -1,18 +1,6 @@
-{-# LANGUAGE ApplicativeDo #-}
-
 module Main (main) where
 
-import Options.Applicative qualified as Options
 import Template qualified
 
-getOptions :: IO Template.Options
-getOptions = do
-  let parserPrefs = Options.prefs mempty
-  let parserInfo =
-        Options.info (Options.helper <*> Template.parseOptions) mempty
-  Options.customExecParser parserPrefs parserInfo
-
 main :: IO ()
-main = do
-  options <- getOptions
-  Template.main options
+main = Template.main
--- src/Template.hs
+++ src/Template.hs
@@ -1,9 +1,4 @@
-module Template
-  ( Options (..)
-  , parseOptions
-  , main
-  )
-where
+module Template (main) where
 
 import Options.Applicative qualified as Options
 
@@ -13,5 +8,14 @@ parseOptions :: Options.Parser Options
 parseOptions = do
   pure Options{}
 
-main :: Options -> IO ()
-main _options = putTextLn "Hello world!"
+getOptions :: IO Template.Options
+getOptions = do
+  let parserPrefs = Options.prefs mempty
+  let parserInfo =
+        Options.info (Options.helper <*> Template.parseOptions) mempty
+  Options.customExecParser parserPrefs parserInfo
+
+main :: IO ()
+main = do
+  _options <- getOptions
+  putTextLn "Hello world!"
